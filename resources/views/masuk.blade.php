9265ab3e37d0edaf99977e226ded81280b38f684
keiishaz <keishzaa124@gmail.com>
2025-05-04 23:55:48 +0700

    Buat view halaman login

---
+++ resources/views/masuk.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left-section">
+            <img src="{{ asset('images/shopp-2.png') }}" alt="Illustration">
+        </div>
+        <div class="right-section">
+            <div class="right-inner">
+            <h2>Masuk</h2>
+            <p>Yuk masuk ke akun kamu dan temukan barang-barang preloved anak kos yang hemat dan bermanfaat!</p>
+            <form action="" method="POST" class="login-form">
+                @csrf
+            
+                <div class="input-group">
+                    <input type="text" name="username" id="username" required autocomplete="off" placeholder="Email">
+                    <span class="icon left"><i class="fa fa-user"></i></span>
+                </div>
+                
+                <div class="input-group">
+                    <input type="password" name="password" id="password-field" required autocomplete="off" placeholder="Kata Sandi">
+                    <span class="icon left"><i class="fa fa-lock"></i></span>
+                    <span class="icon right toggle-password"><i class="fa fa-eye"></i></span>
+                </div>
+                                                      
+                <button type="submit">Masuk</button>
+            </form>
+            
+            <div class="register-prompt">
+                <p>Tidak punya akun? <a href="#">Daftar sekarang!</a></p>
+            </div>
+            
+        </div>
+    </div>
+    <script src="{{ asset('js/login.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('beranda');
 });
+Route::get('/masuk', function () {
+    return view('masuk');
+});
